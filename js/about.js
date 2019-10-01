c01ec94e61536061ce80529ca84462299d896299
lauren-fox <35546993+lauren-fox@users.noreply.github.com>
2019-10-01 18:15:32 -0500

    Add files via upload

---
+++ js/about.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  
+$('body').jGravity({
+  target:'div'
+});
+
+  
+var cursor = document.getElementById('cursor');
+document.addEventListener('mousemove', function(e){
+    var x = e.pageX;
+    var y = e.pageY;
+    cursor.style.left = x + "px";
+    cursor.style.top = y + "px";  
+})
+
+  
+  
+  
+//Closes document ready
+})
+  
